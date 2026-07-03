48f70096200a8b998230e8cf2f9ceff49f0c142a
Munguu15 <Amynga80@gmail.com>
2026-07-03 12:39:38 +0800

    minii index file

---
+++ app/_components/header.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+export default function Header() {
+    return (
+        <head className="w-full border-b border-gray-200 dark: border-neutral-800 bg-white dark:bg-black py-4 sticky top-0 z-50 ">
+            <div className="max-w-7xl mx-auto px-4 ">
+
+            </div>
+        </head>
+    )
+}
